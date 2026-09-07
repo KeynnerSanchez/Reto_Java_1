17ea259f0ed7a3a2a9f79fdabecffae744d4c497
Keynner <keynnersanchez10@gmail.com>
2026-09-07 11:40:38 -0500

    feat: :memo: Base reto
    Base reto

---
+++ src/main/java/com/mycompany/reto_java_1/Reto_Java_1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.reto_java_1;
+
+/**
+ *
+ * @author keynn
+ */
+public class Reto_Java_1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
