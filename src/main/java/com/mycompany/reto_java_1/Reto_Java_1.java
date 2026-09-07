51722d14816e9f0a44c41dd04915442fadf3d8c4
Keynner <keynnersanchez10@gmail.com>
2026-09-07 13:03:13 -0500

    feat: Reto
    Reto

--- src/main/java/com/mycompany/reto_java_1/Reto_Java_1.java
+++ src/main/java/com/mycompany/reto_java_1/Reto_Java_1.java
@@ -10,7 +10,32 @@ package com.mycompany.reto_java_1;
  */
 public class Reto_Java_1 {
 
+   public class ControladorVolumen{
+       private int nivel;
+
+        public ControladorVolumen(int nivel) {
+            this.nivel = 50;
+        }
+
+        public int getNivel() {
+            return nivel;
+        }
+
+        public void setNivel(int nivel) {
+            this.nivel = nivel;
+        }
+       
+       
+   }
+    
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        ControladorVolumen subirVolumen = new ControladorVolumen(70);
+        ControladorVolumen bajarVolumen = new ControladorVolumen(20);
+        if (nivel < 0 || nivel > 100) {
+            System.out.println("Nivel de volumen invalido");
+        }
+        System.out.println("Volument actual: " + nivel);
+        
     }
 }
